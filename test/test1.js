ec9c280a000dc5816a8bc525246ee3574048647f
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-09-04 21:45:21 -0700

    Update test1.js

--- test/test1.js
+++ test/test1.js
@@ -15,11 +15,17 @@ console.time('t1-speed');
 
 // sample pinyin string
 const str1 = 'sheng1 ri4 kuai4 le4';
+console.log(str);
+console.log();
 
 // pbash - indexing pinyin string used in Hanyu Pinyin dictionary or pinyin document search
+console.log('PffyIdioms.pbash("%s")', str1);
 console.log(PffyIdioms.pbash(str1));
+console.log();
 
 // psmash - indexing pinyin string used in Input Method Editors (IMEs)
+console.log('PffyIdioms.psmash("%s")', str1);
 console.log(PffyIdioms.psmash(str1));
+console.log();
 
 console.timeEnd('t1-speed');
